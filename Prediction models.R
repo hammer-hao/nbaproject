aa63fd1d92fe3e334f92eb99642483b22ac1a48b
hammer-hao <haozg@bc.edu>
2022-04-27 20:53:43 -0400

    updated 4.27

--- Prediction models.R
+++ Prediction models.R
@@ -4,12 +4,18 @@ gc()            # Clear memory
 cat("\f")       # Clear the console
 options(scipen = 5) # Remove scientific notation for numbers
 # Prepare needed libraries
-packages <- c("ggplot2"     # Best plotting
-              , "gridExtra" # Arrange multiple plots in a grid
-              , "leaps"     # Best subset selection
-              , "ISLR"      # Textbook datasets
+packages <- c("haven" # To import *.dta files
+              , "ggplot2" # Best plotting
               , "stargazer" # Nice output tables
-              , "glmnet"    # Ridge and Lasso
+              , "car" # For doing F-tests
+              , "sandwich" # For robust standard errors
+              , "lmtest" # For robust standard errors
+              , "boot"
+              , "leaps"
+              , "MASS" # For stepwise selection
+              , "ggrepel"   # For labels in scatter plots
+              , "ISLR"
+              , "glmnet"
 )
 for (i in 1:length(packages)) {
   if (!packages[i] %in% rownames(installed.packages())) {
@@ -23,107 +29,253 @@ for (i in 1:length(packages)) {
 rm(packages)
 
 
-load("/Users/wangyanting/Desktop/Project/nba.full.RData")
+load("nba.full.RData")
 
 
-nba.data <- subset(nba, select = -c(playerid, teamid, season, teamfullsal, player, team, salary.lead, fg, fga, trb, salary.team.total, salary.team.mean, salary.team.median))
-nba.data$position <- as.factor(nba.data$position)
+nba.data <- subset(nba, select = -c(playerid, teamid, season, teamfullsal, player, 
+                                    team, salary.lead, fg, fga, trb, salary.team.total, 
+                                    salary.team.mean, salary.team.median, position))
 nba.data$fg3.pct[nba.data$fg3a==0] <- 0
 nba.data$ft.pct[nba.data$fta==0] <- 0
 nba.data <- na.omit(nba.data)
 
 View(nba.data)
 
+##################################
 
 nba.train <- sample(nrow(nba.data), 0.8*nrow(nba.data))
 data.train <- nba.data[nba.train, ]
 data.test <- nba.data[-nba.train, ]
 
-# Ridge
-nba.ridge1 <- model.matrix(salary ~ ., data.train)[,-1]
-str(nba.ridge1)
-View(nba.ridge1)
+#################################
 
-nba.reg.ridge2<-cv.glmnet(x = nba.ridge1
-                       ,y = data.train$salary
+data.full.train <- model.matrix(salary ~ .^2, data.train)
+View(data.full.train)
+data.full.train <- as.data.frame(data.full.train[,-1]) # -1 to remove the intercept 
+data.full.train$salary <- data.train$salary
+
+# forward selection
+nba.forward <- regsubsets(salary~., data.full.train, 
+                          nvmax=NULL, method="forward"
+                          )
+forward.summary <- summary(nba.forward)
+plot(summary(nba.forward)$adjr2)
+which.max(summary(nba.forward)$adjr2)
+round(coef(nba.forward,which.max(forward.summary$adjr2)),5)
+
+# backward selection
+nba.backward <- regsubsets(salary~., data.full.train, 
+                           nvmax=NULL, method="backward"
+                           )
+
+backward.summary <- summary(nba.backward)
+plot(summary(nba.backward)$adjr2)
+which.max(summary(nba.backward)$adjr2)
+round(coef(nba.backward, which.max(backward.summary$adjr2)),5)
+
+
+get.model.formula <- function(id, reg, outcome){
+  # Identify all variables
+  vars <- summary(reg)$which[id,-1]
+  # Get model predictors used in model with id
+  predictors <- names(which(vars == TRUE))
+  predictors <- paste(predictors, collapse = " + ")
+  # Build model formula
+  formula <- as.formula(paste0(outcome, " ~ ", predictors)) 
+  return(formula)
+} 
+
+get.model.formula(which.max(backward.summary$adjr2)
+                  , nba.backward 
+                  , "salary"
+                  )
+
+
+
+# Now let's compare forward vs backward vs standard OLS
+
+reg.best.forward <- lm(get.model.formula(which.max(forward.summary$adjr2)
+                                         , nba.forward
+                                         , "salary"
+                                        )
+                     , data.full.train
+                     )
+
+summary(reg.best.forward)
+
+reg.best.backward <- lm(get.model.formula(which.max(backward.summary$adjr2)
+                                          , nba.backward
+                                          , "salary"
+                                          )
+                     , data.full.train
+                     )
+
+summary(reg.best.backward)
+
+
+# Standard OLS model with all variables
+reg.best.OLS <- lm(salary ~ ., data.full.train)
+summary(reg.best.OLS)
+
+# Compare models
+stargazer(reg.best.OLS, reg.best.forward, reg.best.backward
+          , column.labels = c("OLS", "Forward", "Backward")
+          , dep.var.labels.include = FALSE
+          , dep.var.caption  = ""
+          , type = "html", style = "default", digits = 2
+          , no.space = TRUE, report = "vc*", omit.stat = c("ser","f", "rsq")
+          , align = TRUE, single.row = TRUE, multicolumn = TRUE
+          , out = "regs.html"
+)
+
+
+# Let's save all coefficient values into a table
+# We will use it later to compare with ridge and lasso regressions
+results.ols <- data.frame(feature = names(reg.best.OLS$coefficients)[-1] # -1 to exclude intercept
+                          , ols = round(coef(reg.best.OLS)[-1], 5) # -1 to exclude intercept
+                         )
+
+results.best.f <- data.frame(feature = names(reg.best.forward$coefficients)[-1]
+                             , best.f = round(coef(reg.best.forward)[-1], 5)
+                            )
+
+results.best.b <- data.frame(feature = names(reg.best.backward$coefficients)[-1]
+                             , best.b = round(coef(reg.best.backward)[-1], 5)
+                            )
+
+# How can we combine these together?
+# We could create a loop to run through one variable at a time
+# So that we can account for different sets of variables between models
+# But a much better way is to do a merge
+results <- merge(results.ols, results.best.f
+                 , by = "feature"
+                 , all.x = TRUE, all.y = TRUE
+)
+
+# And another merge, this time with backward
+results <- merge(results, results.best.b
+                 , by = "feature"
+                 , all.x = TRUE, all.y = TRUE
+)
+
+# Need to remove unnecessary single quotes from feature names
+results$feature <- gsub("`", "", results$feature)
+
+
+
+# Ridge
+nba.ridge <- model.matrix(salary ~ ., data.full.train)[,-1]
+str(nba.ridge)
+View(nba.ridge)
+
+nba.reg.ridge <- cv.glmnet(x = nba.ridge
+                       ,y = data.full.train$salary
                        ,alpha = 0
                        )
 
-coef(nba.reg.ridge2)
+coef(nba.reg.ridge)
 
-plot(nba.reg.ridge2,xvar="lambda",label= TRUE)
-nba.reg.ridge2$lambda.min
-coef(nba.reg.ridge2, s = "lambda.min")
+plot(nba.reg.ridge,xvar="lambda",label= TRUE)
+nba.reg.ridge$lambda.min
+coef(nba.reg.ridge, s = "lambda.min")
 
-# Ridge & interactions
-nba.ridge2 <- model.matrix(salary ~ .^2, data.train)[,-1]
-View(nba.ridge2)
-reg.best.ridge <- cv.glmnet(x = nba.ridge2
-                            , y = data.train$salary
-                            , alpha = 0
-)
-plot(reg.best.ridge)
-reg.best.ridge$lambda.min
-coef(reg.best.ridge, s = "lambda.min")
-
-results.best.ridge <- data.frame(feature = names(coef(reg.best.ridge, s = "lambda.min")[- 1,1])
-                                 , ridge = round(coef(reg.best.ridge, s ="lambda.min")[-1,1], 5)
+results.best.ridge <- data.frame(feature = names(coef(nba.reg.ridge, s = "lambda.min")[- 1,1])
+                                 , ridge = round(coef(nba.reg.ridge, s ="lambda.min")[-1,1], 5)
                                 )
-
-View(results.best.ridge)
+results.best.ridge$feature <- gsub("`", "", results.best.ridge$feature)
 
 results <- merge(results, results.best.ridge
                  , by = "feature"
                  , all.x = TRUE, all.y = TRUE )
 
 # Lasso
-nba.lasso1 <- model.matrix(salary ~ ., data.train)[,-1]
-str(nba.lasso1)
-View(nba.lasso1)
+nba.lasso <- model.matrix(salary ~ ., data.full.train)[,-1]
+str(nba.lasso)
 
-nba.reg.lasso2<-cv.glmnet(x = nba.lasso1
+nba.reg.lasso<-cv.glmnet(x = nba.lasso
                           ,y = data.train$salary
                           ,alpha = 1
                           )
 
-coef(nba.reg.lasso2)
+coef(nba.reg.lasso)
 
-plot(nba.reg.lasso2,xvar="lambda",label= TRUE)
-nba.reg.lasso2$lambda.min
-coef(nba.reg.lasso2, s = "lambda.min")
+plot(nba.reg.lasso,xvar="lambda",label= TRUE)
+nba.reg.lasso$lambda.min
+coef(nba.reg.lasso, s = "lambda.min")
 
-# Lasso & interaction
-nba.lasso2 <- model.matrix(salary ~ .^2, data.train)[,-1]
-View(nba.lasso2)
-reg.best.lasso <- cv.glmnet(x = nba.lasso2
-                            , y = data.train$salary
-                            , alpha = 1
-                            )
-plot(reg.best.lasso)
-reg.best.lasso$lambda.min
-coef(reg.best.lasso, s = "lambda.min")
+results.best.lasso <- data.frame(feature = names(coef(nba.reg.lasso, s = "lambda.min")[- 1,1])
+                                 , lasso = coef(nba.reg.lasso, s ="lambda.min")[-1,1])
 
-results.best.lasso <- data.frame(feature = names(coef(reg.best.lasso, s = "lambda.min")[- 1,1])
-                                 , lasso = coef(reg.best.ridge, s ="lambda.min")[-1,1])
-                                )
+results.best.lasso$feature <- gsub("`", "", results.best.lasso$feature)
 
-View(results.best.lasso)
+results.best.lasso$lasso[results.best.lasso$lasso == 0] <- NA 
+results.best.lasso$lasso <- round(results.best.lasso$lasso, 5)
 
 results <- merge(results, results.best.lasso
                  , by = "feature"
                  , all.x = TRUE, all.y = TRUE )
 
+stargazer(results, summary = FALSE
+          , type = "html", style = "default", digits = 5
+          , no.space = TRUE, report = "vc*", omit.stat = c("ser","f", "rsq")
+          , align = TRUE, single.row = TRUE, multicolumn = TRUE
+          , out = "results.html" )
 
 
-# accuracy
+
+## ACCURACY PART
+
+#create a model matrix object with test data for each regression type
 data.full.test <- model.matrix(salary ~ .^2, data.test)[,-1]
-
+# create a predictions results dataframe
 prediction<- data.frame( salary = data.test$salary)
+
+# Calculate predictions
+# OLS:
+prediction$salary.OLS <- predict(reg.best.OLS, newdata = as.data.frame(data.full.test))
+# Best backward
+prediction$salary.b <- predict(reg.best.backward, newdata = as.data.frame(data.full.test))
+# Best forward
+prediction$salary.f <- predict(reg.best.forward, newdata = as.data.frame(data.full.test))
+
 # 1. Ridge
-prediction$salary.ridge <- as.numeric(predict(reg.best.ridge
-                                                , newx = data.full.test 
+prediction$salary.ridge <- as.numeric(predict(nba.reg.ridge
+                                              , newx = data.full.test 
+                                              , s = "lambda.min"
+                                              , type = "response"
+                                              )
+                                      )
+
+# 2. Lasso
+prediction$salary.lasso <- as.numeric(predict(nba.reg.lasso
+                                                , newx = data.full.test
                                                 , s = "lambda.min"
                                                 , type = "response"
                                               )
-                                        )
+                                      )
+# Calculate MSE:
+accuracy <- data.frame(model = c("OLS"
+                                 , "Backward"
+                                 , "Forward"
+                                 , "Ridge"
+                                 , "Lasso"
+                                )
+                      , MSE = NA
+                      )
+
+accuracy$MSE[accuracy$model == "OLS"] <- mean((prediction$salary - prediction$salary.OLS)^2)
+accuracy$MSE[accuracy$model == "Backward"] <- mean((prediction$salary - prediction$salary.b)^2)
+accuracy$MSE[accuracy$model == "Forward"] <- mean((prediction$salary - prediction$salary.f)^2)
+accuracy$MSE[accuracy$model == "Ridge"] <- mean((prediction$salary - prediction$salary.ridge)^2)
+accuracy$MSE[accuracy$model == "Lasso"] <- mean((prediction$salary - prediction$salary.lasso)^2)
+
+# And the winner is....
+accuracy
+
+# Relative MSE of everybody with OLS as a baseline
+accuracy$MSE.relative <- accuracy$MSE/accuracy$MSE[accuracy$model == "OLS"]
+accuracy
+
+
+
 
